db90888f954e0d925d46b60310b657670fb53b3f
Gromnax <hezon.morgan@orange.fr>
2025-08-02 17:35:40 +0200

    Adds pushing the kart when colliding with the barrel

--- scenes/obstacles/explosive_barrel.gd
+++ scenes/obstacles/explosive_barrel.gd
@@ -2,6 +2,9 @@ extends RigidBody3D
 
 const Explosion : PackedScene = preload("res://addons/PolyBlocks/EffectBlocks/assets/explosions/explosion_small.tscn")
 # Called when the node enters the scene tree for the first time.
+
+var local_collision_pos : Vector3 = Vector3(0,0,0)
+
 func _ready() -> void:
 	pass # Replace with function body.
 
@@ -21,5 +24,12 @@ func _on_body_entered(body: Node3D) -> void:
 	if not(body is RigidBody3D):
 		pass
 	print("Body entered:"+str(body))
-	if body.get_collision_layer_value(1):
-		explode()
+	if body is RigidBody3D:
+		print("True")
+		if body.get_collision_layer_value(1):
+			print("Also True")
+			var explosion_vector : Vector3 = global_position - body.global_position
+			var explosion_power : float = -20000000.0
+			body.apply_central_impulse(explosion_vector*explosion_power)
+			explode()
+		
