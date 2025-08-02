7e1e87dabb0567b79ce4889e97b7852595aa65ba
melchior06 <melchior.bourdin@icloud.com>
2025-08-02 20:36:50 +0200

    fix gameplay + sons + modele de la voiture

--- addons/gevp/scripts/camera.gd
+++ addons/gevp/scripts/camera.gd
@@ -8,17 +8,34 @@ extends Camera3D
 
 var start_rotation : Vector3
 var start_position : Vector3
+var shake_strength: float = 0.0
+var shake_decay: float = 5.0
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+var original_position: Vector3
 
 func _ready():
 	start_rotation = rotation
 	start_position = position
+	original_position = position
+	rng.randomize()
 
 func _physics_process(delta : float):
-	var delta_v := global_transform.origin - follow_this.global_transform.origin
+	var delta_v: Vector3 = global_transform.origin - follow_this.global_transform.origin
 	delta_v.y = 0.0
 	if delta_v.length() > follow_distance:
 		delta_v = delta_v.normalized() * follow_distance
 		delta_v.y = follow_height
 		global_position = follow_this.global_transform.origin + delta_v + Vector3.UP * camera_y_offset
 
+	var offset: Vector3 = Vector3.ZERO
+	if shake_strength > 0.01:
+		shake_strength = lerp(shake_strength, 0.0, delta * shake_decay)
+		offset.x = rng.randf_range(-shake_strength, shake_strength)
+		offset.y = rng.randf_range(-shake_strength, shake_strength)
+
+	global_position += offset
+
 	look_at(follow_this.global_transform.origin + Vector3.UP * camera_y_offset, Vector3.UP)
+
+func trigger_shake(strength: float) -> void:
+	shake_strength = strength
--- scenes/obstacles/explosive_barrel.gd
+++ scenes/obstacles/explosive_barrel.gd
@@ -1,35 +1,30 @@
 extends RigidBody3D
 
-const Explosion : PackedScene = preload("res://addons/PolyBlocks/EffectBlocks/assets/explosions/explosion_small.tscn")
-# Called when the node enters the scene tree for the first time.
+const Explosion: PackedScene = preload("res://addons/PolyBlocks/EffectBlocks/assets/explosions/explosion_small.tscn")
 
-var local_collision_pos : Vector3 = Vector3(0,0,0)
+var local_collision_pos: Vector3 = Vector3(0, 0, 0)
+var game_manager: Node = null
 
 func _ready() -> void:
-	pass # Replace with function body.
+	game_manager = get_tree().get_root().find_child("GameManager", true, false)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func explode() -> void:
-	var explosion : Node3D = Explosion.instantiate()
+	var explosion: Node3D = Explosion.instantiate()
 	add_child(explosion)
 	explosion.explosion()
 	queue_free()
 
 func _on_body_entered(body: Node3D) -> void:
-	if not(body is RigidBody3D):
-		pass
-	print("Body entered:"+str(body))
-	if body is RigidBody3D:
-		print("True")
-		if body.get_collision_layer_value(1):
-			print("Also True")
-			var explosion_vector : Vector3 = global_position - body.global_position
-			var explosion_power : float = -20000000.0
-			body.apply_central_impulse(explosion_vector*explosion_power)
-			explode()
-		
+	if not (body is RigidBody3D):
+		return
+
+	if body.get_collision_layer_value(1):
+		var explosion_vector: Vector3 = global_position - body.global_position
+		var explosion_power: float = -20000.0
+		body.apply_central_impulse(explosion_vector * explosion_power)
+		if game_manager:
+			game_manager.lose_life()
+		explode()
