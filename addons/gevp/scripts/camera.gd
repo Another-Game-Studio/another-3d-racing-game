f69e21047a8aa2d02095313ae7515e047efb2473
melchior06 <melchior.bourdin@icloud.com>
2025-08-02 16:29:56 +0200

    Test gameplay (#13)

--- addons/gevp/scripts/camera.gd
+++ addons/gevp/scripts/camera.gd
@@ -1,15 +1,9 @@
 extends Camera3D
 
-## The distance, in meters, that the camera will be from the
-## [Node3D] in [member follow_this].
 @export var follow_distance = 5
-## The height, in meters, that the camera will be from the
-## [Node3D] in [member follow_this].
 @export var follow_height = 2
-## The speed, in meters per second, that the camera will
-## move to reach the [Node3D] in [member follow_this].
-@export var speed:=20.0
-## The [Node3D] that the camera will follow.
+@export var camera_y_offset := 1.0
+@export var speed := 20.0
 @export var follow_this : Node3D
 
 var start_rotation : Vector3
@@ -22,9 +16,9 @@ func _ready():
 func _physics_process(delta : float):
 	var delta_v := global_transform.origin - follow_this.global_transform.origin
 	delta_v.y = 0.0
-	if (delta_v.length() > follow_distance):
+	if delta_v.length() > follow_distance:
 		delta_v = delta_v.normalized() * follow_distance
 		delta_v.y = follow_height
-		global_position = follow_this.global_transform.origin + delta_v
-	
-	look_at(follow_this.global_transform.origin, Vector3.UP)
+		global_position = follow_this.global_transform.origin + delta_v + Vector3.UP * camera_y_offset
+
+	look_at(follow_this.global_transform.origin + Vector3.UP * camera_y_offset, Vector3.UP)
