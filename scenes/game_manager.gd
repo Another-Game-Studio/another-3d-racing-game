e04aa8a6146424b0466f964830b8547fa894d697
melchior06 <melchior.bourdin@icloud.com>
2025-08-02 21:11:04 +0200

    modele 3D + sons + scale (#22)
    Co-authored-by: Gromnax <morgan.hezon@outlook.fr>

--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -1,30 +1,41 @@
 extends Node
 
 @export var coin_label: Label
-
 @export var heart_sprites: Array[Sprite2D]
+@export var heart_full: Texture2D
 @export var heart_empty: Texture2D
+@export var coin_sound: AudioStream
+@export var damage_sound: AudioStream
 
 var coin_count: int = 0
 var life: int = 4
 
+@onready var coin_player: AudioStreamPlayer = AudioStreamPlayer.new()
+@onready var damage_player: AudioStreamPlayer = AudioStreamPlayer.new()
 
 func _ready() -> void:
+	coin_player.volume_db = linear_to_db(0.5)
+	damage_player.volume_db = linear_to_db(0.5)
+	add_child(coin_player)
+	add_child(damage_player)
 	SignalBus.coin_collected.connect(_on_coin_collected)
 	_update_label()
-
 	_update_hearts()
-
 
 func _on_coin_collected() -> void:
 	coin_count += 1
 	_update_label()
-
+	if coin_sound:
+		coin_player.stream = coin_sound
+		coin_player.play()
 
 func lose_life() -> void:
 	life -= 1
 	life = max(life, 0)
 	_update_hearts()
+	if damage_sound:
+		damage_player.stream = damage_sound
+		damage_player.play()
 
 func _update_label() -> void:
 	if coin_label:
@@ -32,5 +43,7 @@ func _update_label() -> void:
 
 func _update_hearts() -> void:
 	for i: int in heart_sprites.size():
-		if i >= life:
+		if i < life:
+			heart_sprites[i].texture = heart_full
+		else:
 			heart_sprites[i].texture = heart_empty
