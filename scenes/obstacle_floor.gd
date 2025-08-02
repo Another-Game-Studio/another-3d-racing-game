e2e434b7f39490e5d052cb022375ba246edc7e23
Gromnax <hezon.morgan@orange.fr>
2025-08-02 23:11:08 +0200

    Revert "Merge branch 'main' into birb"
    This reverts commit 130ffda0021c2005c4c8f1c706fb9a3f15145ede, reversing
    changes made to 55ff3289aeef82901491c6e62d249eaf76e1647c.

--- scenes/obstacle_floor.gd
+++ scenes/obstacle_floor.gd
@@ -1,11 +1,11 @@
 extends Area3D
 
-@export var troncon_manager: Node3D
+@export var game_manager: Node
 
 func _on_body_entered(body: Node) -> void:
 	if not (body is PhysicsBody3D):
 		return
+
 	if body.get_collision_layer_value(1):
-		print("TRIGGER")
-		if troncon_manager:
-			troncon_manager.change_scene()
+		if game_manager:
+			game_manager.lose_life()
--- scenes/troncon_manager.gd
+++
@@ -1,8 +0,0 @@
-extends Node3D
-
-@export var new_scene: PackedScene
-
-func change_scene() -> void:
-	if new_scene:
-		var instance: Node3D = new_scene.instantiate()
-		add_child(instance)
