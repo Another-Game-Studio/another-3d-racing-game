66060ca5f493e0f1b9d013e289f763e4f7b5133f
Jack-Kaller <jackson-kaller@proton.me>
2025-08-03 17:40:28 +0200

    fix score on car fall

--- scenes/death_zone/deadzone.gd
+++ scenes/death_zone/deadzone.gd
@@ -9,4 +9,4 @@ func _on_body_entered(body: Node3D) -> void:
 			call_deferred("_go_to_game_over")
 
 func _go_to_game_over() -> void:
-	get_tree().change_scene_to_packed(game_over_scene)
+	SignalBus.car_fell.emit()
