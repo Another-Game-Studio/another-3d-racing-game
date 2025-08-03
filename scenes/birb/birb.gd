4d9f66b235899139d2b816148dc4af58bde85807
Gromnax <morgan.hezon@outlook.fr>
2025-08-03 11:28:57 +0200

    Birb (#36)

--- scenes/birb/birb.gd
+++ scenes/birb/birb.gd
@@ -3,11 +3,40 @@ class_name Birb
 
 @onready var droppoints : Array[Marker3D] = []
 
+
+const coin_class : PackedScene = preload("res://scenes/collectibles/coin/Coin.tscn")
+
+var can_drop : bool = false :
+	set(new_value):
+		can_drop = new_value
+		if can_drop:
+			$CoinDropTimer.start()
+		else:
+			$CoinDropTimer.stop()
+
+## Le monde 3D dans lequel lâcher notre pièce
+var world_3D : Node3D
+
+
 func _ready() -> void:
 	var droppoints_nodes : Array[Node] = $Droppoints.get_children()
 	for droppoint_node : Node in droppoints_nodes:
 		if droppoint_node is Marker3D:
 			droppoints.append(droppoint_node as Marker3D)
+
+	$CoinDropTimer.timeout.connect(_on_drop_timer_timeout)
+
+func _on_drop_timer_timeout() -> void:
+	drop_coin()
+	$CoinDropTimer.start()
+
+func drop_coin() -> void:
+	if !is_inside_tree():
+		pass
+	var coin : RigidBody3D = coin_class.instantiate()
+	world_3D.add_child(coin)
+	coin.global_position = $Droppoints.get_random_droppoint()
+
 
 func _physics_process(delta: float) -> void:
 	pass
--- scenes/collectibles/coin/coin.gd
+++ scenes/collectibles/coin/coin.gd
@@ -7,11 +7,12 @@ var time : float = 0.0
 
 @onready var start_y : float = global_position.y
 @export var whole_coin: RigidBody3D
+
 func _process(delta : float) -> void:
 	rotate(Vector3.UP, spin*delta)
 	time+=delta
 	var d : float = (sin(time*bob_speed)+1)/2
-	global_position.y = start_y + (d * bob_height)
+	global_position.y = whole_coin.position.y + (d * bob_height)
 
 func _on_body_entered(body : Node) -> void:
 	if body is PhysicsBody3D:
