99bfe754636baf661e10f42f311fbe2ee1cb7eb4
melchior06 <melchior.bourdin@icloud.com>
2025-08-03 14:49:29 +0200

    Title screen + options + credits UI (#37)

--- menus/credits/main_menu_button.gd
+++ menus/credits/main_menu_button.gd
@@ -4,4 +4,4 @@ func _ready() -> void:
 	button_down.connect(_on_button_down)
 	
 func _on_button_down() -> void:	
-	get_tree().change_scene_to_file("res://menus/main_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
